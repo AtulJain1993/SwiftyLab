a565ac2f5732dd7e6bb3ab10a8f508c9ceda8b3b
Atul Apporio <atul@apporio.com>
2020-03-17 17:40:37 +0530

    Initial Commit

---
+++ SwiftyLib/SwiftyLib.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftyLib.swift
+//  SwiftyLib
+//
+//  Created by Atul Jain on 17/03/20.
+//  Copyright © 2020 Atul Jain. All rights reserved.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
