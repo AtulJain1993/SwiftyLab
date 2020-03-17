9f77132e2c83783365b0d6cbd95e465cd7247e19
Atul Apporio <atul@apporio.com>
2020-03-17 16:30:13 +0530

    Initial Commit

---
+++ SwiftyLib/SwiftyLib.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyLib.h
+//  SwiftyLib
+//
+//  Created by Atul Jain on 17/03/20.
+//  Copyright © 2020 Atul Jain. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyLib.
+FOUNDATION_EXPORT double SwiftyLibVersionNumber;
+
+//! Project version string for SwiftyLib.
+FOUNDATION_EXPORT const unsigned char SwiftyLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyLib/PublicHeader.h>
+
+
