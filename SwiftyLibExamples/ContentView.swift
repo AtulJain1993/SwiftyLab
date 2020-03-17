0450f2cb2afbcacba3cda2a6b76019871ee26695
Atul Apporio <atul@apporio.com>
2020-03-17 16:31:48 +0530

    first commit

---
+++ SwiftyLibExamples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftyLibExamples
+//
+//  Created by Atul Jain on 17/03/20.
+//  Copyright © 2020 Atul Jain. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
